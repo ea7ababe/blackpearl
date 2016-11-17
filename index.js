43453e237e923a9f0899bd4dc8548a197cf8fc8a
Basilius Barbanera <businesspig@yandex.com>
2016-11-17 16:20:07 +0200

    Add --tmp flag to specify download directory.

--- index.js
+++ index.js
@@ -8,37 +8,39 @@ var chalk    = require('chalk');
 var action   = process.argv[2];
 var name     = process.argv[3];
 var actions  = ["search"];
-    
+
 var subs_language;
 
-var help_screen = function(){
+var help_screen = function() {
 
 	var path = require('path').resolve(__dirname);
 
 	fs.readFile(path + '/' + 'logo.dat', 'utf8', function (err,data) {
 	  	console.log(chalk.yellow(data));
-		console.log(chalk.cyan("Usage: blackpearl search [name]"));
+		console.log(chalk.cyan("Usage: blackpearl search [name] [--tmp dir]"));
 		console.log(chalk.red("Subtitles: --sub [spa,eng,fre,etc]"));
-		process.exit(); 
-	}); 
-	
+		process.exit();
+	});
+
 }
 
-if( actions.indexOf(action) == -1){
+if (actions.indexOf(action) == -1) {
+	console.log(action)
 	return help_screen();
 }
 
-if ( process.argv.indexOf("--sub") != -1 && process.argv[ process.argv.indexOf("--sub") + 1 ] ) {
-	subs_language = process.argv[ process.argv.indexOf("--sub") + 1 ];
+var sub = process.argv.indexOf("--sub") + 1;
+var tmp = process.argv.indexOf("--tmp") + 1;
+var flags = {
+	subs_lang: sub ? process.argv[sub] : "",
+	tmp_dir: tmp ? process.argv[tmp] : "",
 }
 
 async.waterfall([
-    async.apply(tpb.search, name, subs_language),
+    async.apply(tpb.search, name, flags),
     cli.showResults,
     cli.chooseResult,
     subs.searchSub,
     subs.downloadSub,
     stream.startStreaming
 ]);
-
-
